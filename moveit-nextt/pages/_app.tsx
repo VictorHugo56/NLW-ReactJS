ff1c95c4170fb95cd19b55d3c855289695cd1190
Victor Hugo <victor.pereira2056@gmail.com>
2023-10-14 19:59:18 -0300

    updating...

---
+++ moveit-nextt/pages/_app.tsx
@@ -0,0 +1,8 @@
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+function MyApp({  Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
