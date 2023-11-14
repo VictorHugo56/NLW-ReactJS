b7693b0460fb34b35d08c6236ab22e16e45ab63b
Victor Hugo <victor.pereira2056@gmail.com>
2023-11-14 18:22:03 -0300

    in progress

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/global.css'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return ( 
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
