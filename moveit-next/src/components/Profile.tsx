18c52e5a9e36f90ff987d240cb348bc06a94ccc7
Victor Hugo <victor.pereira2056@gmail.com>
2023-11-18 20:55:54 -0300

    ...

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { ChallengesContext } from '@/contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
+import { useContext } from 'react';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/VictorHugo56.png" alt="Victor Hugo" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Victor Hugo</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -18,6 +18,7 @@ interface ChallengesContextData {
     levelUp: () => void;
     startNewChallenge: () => void;
     resetChallenge: () => void;
+    completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -51,6 +52,25 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         setActiveChallenge(null);
     }
 
+    function completeChallenge () {
+        if (!activeChallenge) {
+            return;
+        }
+
+        const { amount } = activeChallenge;
+
+        let finalExperience = currentExperience + amount;
+
+        if (finalExperience >= experienceToNextLevel) {
+            finalExperience = finalExperience - experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
+    }
+
     return (
     <ChallengesContext.Provider
         value={{ 
@@ -58,6 +78,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
             challengesCompleted, levelUp,
             startNewChallenge, activeChallenge,
             resetChallenge, experienceToNextLevel,
+            completeChallenge,
         }}>
         {children}
     </ChallengesContext.Provider>
